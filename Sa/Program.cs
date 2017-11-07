bf175fc4b9269d10374f1bf3db5a415ee0f75c8b
dim777 <dim777@ya.ru>
2017-11-07 08:33:21 +0300

    Init

---
+++ Sa/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NLog;
+
+namespace Sa
+{
+    class Program
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        
+        static void Main(string[] args)
+        {
+            logger.Info("Starting app . . . ");
+        }
+    }
+}
